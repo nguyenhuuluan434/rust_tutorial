e3645a439971ec60025a0bd3ecfbc7cb861549b9
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2020-10-07 13:34:12 +0700

    commit data type.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,50 @@
-use std::cmp::Ordering;
-use std::io::stdin;
-
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number");
-    let mut guess;
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    loop {
-        println!("Please input your guess");
-        guess = String::new();
-        stdin().read_line(&mut guess).expect("Fail to read line");
-        println!("You guessed {}", guess);
-        /**/
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            }
-        }
-        println!("The secret number is {}", secret_number);
-    }
+    /*by default variables are immutable.*/
+    /* by adding **mut** in front of the variable name to make them mutable*/
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = x + 1;
+    println!("The value of x is: {}", x);
+
+    /*Constants aren’t just immutable by default—they’re always immutable.*/
+    const MAX_POINTS: u32 = 100_000;
+
+    /*Shadowing,you can declare a new variable with the same name as a previous variable, and the new variable shadows the previous variable */
+    /*Shadow allow assign new data to variable with same name, the mutable variable is not*/
+    let spaces = "   ";
+    println!("{}", spaces);
+    let spaces = spaces.len();
+    println!("{}", spaces);
+
+    /*Data Type*/
+    /*Every value in Rust is of a certain data type, which tell rust how to work with that data*/
+    let guess: i128 = "42".parse().expect("Not a number!");
+
+    let decimal: i128 = 89_222_333_444_444_444_4444_444;
+    println!("{}", decimal);
+
+    let decimal = 100.0 / 2.4;
+    println!("{}", decimal);
+
+    let heart_eyed_cat = '😻';
+    println!("{}", heart_eyed_cat);
+
+    /*Compound Types*/
+
+    /*The Tuple Type*/
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("{}", tup.0);
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    /*Can function return multiple value?*/
+    println!("The value of y is: {}", y);
+
+    /*The Array Type*/
+    /*Arrays are useful when you want your data allocated on the stack rather than the heap*/
+    /* An array isn’t as flexible as the vector type*/
+    /**/
+    let a = [1, 2, 3, 4, 5];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [3; 5];/*let a = [3, 3, 3, 3, 3];*/
 }
