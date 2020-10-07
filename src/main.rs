29b363a2180db04ee94c0e460eb553e1dd77edcf
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2020-10-07 14:12:44 +0700

    commit function

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,32 @@
+/*Function definitions in Rust start with fn and have a set of parentheses after the function name.*/
 fn main() {
-    /*by default variables are immutable.*/
-    /* by adding **mut** in front of the variable name to make them mutable*/
-    let mut x = 5;
+    println!("Hello, world!");
+    another_function(5, 9);
+    function_expression();
+    let mul = multi(10, 20);
+    println!("{:?}", mul);
+}
+
+/*fn another_function() {
+    println!("Another function.");
+}*/
+/*Functions can also be defined to have parameters, which are special variables that are part of a function’s signature.*/
+fn another_function(x: i32, y: i64) {
     println!("The value of x is: {}", x);
-    x = x + 1;
+    println!("The value of x is: {}", y);
+}
+
+fn function_expression() {
+    let x: i32 = 0;
+    let y = {
+        let x = 4;
+        x + 1;
+    };
     println!("The value of x is: {}", x);
+    println!("The value of x is: {:?}", y);
+}
 
-    /*Constants aren’t just immutable by default—they’re always immutable.*/
-    const MAX_POINTS: u32 = 100_000;
-
-    /*Shadowing,you can declare a new variable with the same name as a previous variable, and the new variable shadows the previous variable */
-    /*Shadow allow assign new data to variable with same name, the mutable variable is not*/
-    let spaces = "   ";
-    println!("{}", spaces);
-    let spaces = spaces.len();
-    println!("{}", spaces);
-
-    /*Data Type*/
-    /*Every value in Rust is of a certain data type, which tell rust how to work with that data*/
-    let guess: i128 = "42".parse().expect("Not a number!");
-
-    let decimal: i128 = 89_222_333_444_444_444_4444_444;
-    println!("{}", decimal);
-
-    let decimal = 100.0 / 2.4;
-    println!("{}", decimal);
-
-    let heart_eyed_cat = '😻';
-    println!("{}", heart_eyed_cat);
-
-    /*Compound Types*/
-
-    /*The Tuple Type*/
-
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    println!("{}", tup.0);
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    /*Can function return multiple value?*/
-    println!("The value of y is: {}", y);
-
-    /*The Array Type*/
-    /*Arrays are useful when you want your data allocated on the stack rather than the heap*/
-    /* An array isn’t as flexible as the vector type*/
-    /**/
-    let a = [1, 2, 3, 4, 5];
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    let a = [3; 5];/*let a = [3, 3, 3, 3, 3];*/
+/*Functions can return values to the code that calls them*/
+fn multi(x: i32, y: i32) -> i32 {
+    return x * y;
 }
