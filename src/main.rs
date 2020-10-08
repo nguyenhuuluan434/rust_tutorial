bf4c7e2fba6810d639ea6cf9a1f7d15f50abb3f2
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2020-10-08 16:29:40 +0700

    slice to warranty the reference valid

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 fn main() {
-    let s1 = String::from("hello");
-    println!("{:p}", &s1);
-    let len = calculate_length(&s1);
-    println!("{:p}", &s1);
-    println!("The length of '{}' is {}.", s1, len);
-    let mut s = String::from("hello");
+    let mut s = String::from("hello worl");
+    let x = first_word(&s);
+    println!("{}", x);
+    //s.clear();
+    let s = "luannh";
+    println!("{}", x);
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    println!("{:?}", slice);
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{:p}, {:p},", r1, r2);
-    let r3 = &mut s; // no problem
-    println!("{:p}", r3);
 }
 
-fn calculate_length(s: &String) -> usize {
-    println!("{:p}", s);
-    s.len()
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    println!("{:?}", bytes);
+    for (i, &item) in bytes.iter().enumerate() {
+        if item != b' ' {
+            continue;
+        }
+        return &s[0..i];
+    }
+    &s[..]
 }
